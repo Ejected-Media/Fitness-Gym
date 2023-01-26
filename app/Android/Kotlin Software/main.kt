649367defbcfd59b9d78ae25f144759266561366
Kyle2501 <kyle2501@gmail.com>
2023-01-26 10:05:03 -0700

    . added folder and code file for Swift on Mac Desktop

--- app/Android/Kotlin Software/main.kt
+++ app/Android/Kotlin Software/main.kt
@@ -1,2 +1,9 @@
 // . hi
 
+val jellyfin = createJellyfin {
+    clientInfo = ClientInfo(name = "My awesome client!", version = "1.33.7",)
+
+    // Uncomment when using android:
+    // context = /* Android Context */
+}
+
